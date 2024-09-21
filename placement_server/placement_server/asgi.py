ce1dac53b06cc205d624d76ae227f446f1b1ed00
KAsare1 <asarek977@gmail.com>
2024-09-21 11:37:34 +0000

    first commit-5

--- placement_server/placement_server/asgi.py
+++ placement_server/placement_server/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'placement_server.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'placement_server.settings.production')
 
 application = get_asgi_application()
