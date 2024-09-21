ad57864f5f0953a2f7786edb7dc5d7d230889ca6
KAsare1 <asarek977@gmail.com>
2024-09-21 11:30:24 +0000

    first commit-5

--- placement_server/manage.py
+++ placement_server/manage.py
@@ -11,7 +11,7 @@ def main():
     if base.DEBUG:
         os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'placement_server.settings.local')
     else:
-        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'placement_server.settings.production')
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'placement_server.placement_server.settings.production')
 
     try:
         from django.core.management import execute_from_command_line
